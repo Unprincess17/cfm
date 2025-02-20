79e9196371f97a695a23ecfa8934b9683a8d4bba
Gong Shufan <2624542821@qq.com>
2025-02-20 21:25:42 +0800

    add setup script

---
+++ setup/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# faasnap
+git submodule update --init  --recursive && \
+pushd faasnap && \
+sudo apt install -y redis-server && \
+pip install grpcio grpcio-tools numpy scipy scikit-learn psutil redis flask && \
+python populate_resource.py && \
+popd && \
+
+pushd quicksort && make && popd && \
+
+# spark
+pushd ~ && \
+wget https://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz && \
+tar -xzf spark-2.4.0-bin-hadoop2.7.tgz && \
+rm spark-2.4.0-bin-hadoop2.7.tgz
+sudo apt install openjdk-8-jdk && \
+mkdir -p ~/spark-2.4.0-bin-hadoop2.7/data/sosp/ && \
+tar -zxvf web-BerkStan.txt.tar.gz -C ~/spark-2.4.0-bin-hadoop2.7/data/sosp/ && \
+popd
+
+pushd spark/pagerank/ && \
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list && \
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list && \
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823 && \
+sudo apt-get update && \
+sudo apt-get install sbt && \
+sbt compile && \
+# In my case, only after run, I got the pagerank jar file
+sbt run && \
+popd && \
+
+pushd stream && make && popd
+
